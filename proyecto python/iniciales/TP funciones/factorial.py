3267fe10b64f3761e8f7158684f00ab5f5ea03b7
Daniel <sosaaquinopd@gmail.com>
2026-02-16 00:13:23 -0300

    ejercicio factorial.py

---
+++ proyecto python/iniciales/TP funciones/factorial.py
@@ -0,0 +1,12 @@
+# Factorial
+# Escribir una función que reciba un número entero po# sitivo y devuelva su factori# al
+
+num : int
+
+def factorial(num) -> int:
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+print(f'{factorial(4)}')

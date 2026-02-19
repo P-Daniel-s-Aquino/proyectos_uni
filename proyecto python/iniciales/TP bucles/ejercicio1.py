fd405974e87e08a66bcf170073485eef324f7508
Daniel <sosaaquinopd@gmail.com>
2026-02-19 20:58:45 -0300

    modificacion ejercicio 1 tp bucles

--- proyecto python/iniciales/TP bucles/ejercicio1.py
+++ proyecto python/iniciales/TP bucles/ejercicio1.py
@@ -1,7 +1,7 @@
 #Escribir un programa que muestre el eco de todo
 #lo que el usuario introduzca hasta que el usuario escriba "salir"
 
-word = input("ingresa lo que se te cante, de lo contrario ingrese salir: ")
+word : str = input("ingresa lo que se te cante, de lo contrario ingrese salir: ")
 
 while word != "salir":
     print(word)

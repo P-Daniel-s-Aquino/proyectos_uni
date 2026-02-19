c17fd17d3c8463caee45cca7c94317da0b9085dd
Daniel <sosaaquinopd@gmail.com>
2026-02-19 14:49:48 -0300

    modificaciones ejercicio3.py

--- proyecto python/iniciales/TP funciones/ejercicio3.py
+++ proyecto python/iniciales/TP funciones/ejercicio3.py
@@ -1,7 +1,7 @@
 # Factorial
 # Escribir una función que reciba un número entero po# sitivo y devuelva su factori# al
 
-num : int
+num : int = int(input('ingrese un numero entero: '))
 
 def factorial(num) -> int:
     if num == 0 or num == 1:
@@ -9,4 +9,4 @@ def factorial(num) -> int:
     else:
         return num * factorial(num - 1)
 
-print(f'{factorial(4)}')
+print(f'{factorial(num)}')
